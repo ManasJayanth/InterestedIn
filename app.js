6f03634bd2be29b3226e9836ee95184cc38e43bc
prometheansacrifice <harlequinsguitar@gmail.com>
2014-02-03 15:15:19 +0530

    app.js modified

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express.createServer();
 app.configure(function(){
-app.set('view engine', 'jade');
-app.use(express.static(__dirname + '/public'));
+    app.set('view engine', 'jade');
+    app.use(express.static(__dirname + '/public'));
 });
 app.get('/', function(req, res){
-res.render("index.jade", {layout:false});
+    res.render("index.jade", {layout:false});
 });
 app.listen(8080);
