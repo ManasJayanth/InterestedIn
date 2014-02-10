423d92f87e0d87dd3e5d381dd70ea242f3ac0042
prometheansacrifice <harlequinsguitar@gmail.com>
2014-02-10 10:53:42 +0530

    Added cookieParser() and session() to expresss

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.cookieParser());
+app.use(express.session({secret: '1234567890QWERTY'}));
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
